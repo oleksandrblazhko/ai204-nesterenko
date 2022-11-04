b653759d9d947a914fce5b284b68dd16d55eee24
bezpechno <52717082+bezpechno@users.noreply.github.com>
2022-11-04 10:49:31 +0200

    Update Data.java

--- 3-SoftwareConstruction/3-Program-Classes-Example/Data.java
+++ 3-SoftwareConstruction/3-Program-Classes-Example/Data.java
@@ -1,1 +1,56 @@
 
+package com.example.demo.domain;
+
+import java.math.BigInteger;
+
+public class Data {
+    private int dataId;
+    private Float volume;
+    private String feeling;
+    private String text;
+
+    public int getDataId() {
+        return dataId;
+    }
+
+    public Data(Float volume, String feeling, String text) {
+        this.volume = volume;
+        this.feeling = feeling;
+        this.text = text;
+    }
+
+    public void setDataId(int dataId) {
+        this.dataId = dataId;
+    }
+
+    public Float getVolume() {
+        return volume;
+    }
+
+    public void setVolume(Float volume) {
+        this.volume = volume;
+    }
+
+    public String getFeeling() {
+        return feeling;
+    }
+
+    public void setFeeling(String feeling) {
+        this.feeling = feeling;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setData(Float volume, String feeling, String text){
+        this.volume = volume;
+        this.feeling = feeling;
+        this.text = text;
+    }
+
+}
